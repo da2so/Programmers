038a7d5b685ee9ef4de3882bb853bee431c7b43e
gangsinhan <kangsinhan@nate.com>
2020-05-11 19:44:06 +0900

    Update "완주하지못한선수"

---
+++ 완주하지못한선수/main.py
@@ -0,0 +1,9 @@
+from 완주하지못한선수.Solution import solution
+
+
+if __name__ == "__main__":
+    participan=["mislav", "stanko", "mislav", "ana"]
+    completion=["stanko", "ana", "mislav"]
+
+
+    print(solution(participan,completion))
