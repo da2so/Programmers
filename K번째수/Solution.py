05ad8353c7d672665c90a4cef32142cd7ab1efec
gangsinhan <kangsinhan@nate.com>
2020-05-12 13:25:09 +0900

    Update "K번째수"

---
+++ K번째수/Solution.py
@@ -0,0 +1,51 @@
+"""
+배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+
+예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+
+array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+2에서 나온 배열의 3번째 숫자는 5입니다.
+배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+array의 길이는 1 이상 100 이하입니다.
+array의 각 원소는 1 이상 100 이하입니다.
+commands의 길이는 1 이상 50 이하입니다.
+commands의 각 원소는 길이가 3입니다.
+입출력 예
+array	commands	return
+[1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+입출력 예 설명
+[1, 5, 2, 6, 3, 7, 4]를 2번째부터 5번째까지 자른 후 정렬합니다. [2, 3, 5, 6]의 세 번째 숫자는 5입니다.
+[1, 5, 2, 6, 3, 7, 4]를 4번째부터 4번째까지 자른 후 정렬합니다. [6]의 첫 번째 숫자는 6입니다.
+[1, 5, 2, 6, 3, 7, 4]를 1번째부터 7번째까지 자릅니다. [1, 2, 3, 4, 5, 6, 7]의 세 번째 숫자는 3입니다.
+"""
+def solution(array, commands):
+    answer = []
+
+
+    def quick_sort(arr):
+        if len(arr) <= 1:
+            return arr
+        pivot=len(arr)//2
+        lesser_arr,equal_arr,greater_arr=[],[],[]
+
+        for i in range(len(arr)):
+            if arr[pivot] <arr[i]:
+                greater_arr.append(arr[i])
+            elif arr[pivot] >arr[i]:
+                lesser_arr.append(arr[i])
+            else:
+                equal_arr.append(arr[i])
+        return quick_sort(lesser_arr)+equal_arr+quick_sort(greater_arr)
+
+    for i in range(len(commands)):
+        tmp_array=array[commands[i][0]-1:commands[i][1]]
+
+        tmp_array=quick_sort(tmp_array)
+
+        answer.append(tmp_array[commands[i][2]-1])
+    return answer
+
+
---
+++ K번째수/main.py
@@ -0,0 +1,8 @@
+from K번째수.Solution import solution
+
+
+if __name__ == "__main__":
+    array=[1, 5, 2, 6, 3, 7, 4]
+    commands=[[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+
+    print(solution(array,commands))
